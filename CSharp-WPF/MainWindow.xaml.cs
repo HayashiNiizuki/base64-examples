44b83593f707cdd3e2c4acc49719c1d60c4aeb82
Hayashi <33160326+HayashiNiizuki@users.noreply.github.com>
2024-03-02 21:36:38 +0800

    WPF demo

---
+++ CSharp-WPF/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+
+namespace CSharp_WPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void DecodeButton_Click(object sender, RoutedEventArgs e)
+        {
+            String Text = Base64Box.Text.Trim();
+            Byte[] Bytes = Convert.FromBase64String(Text);
+            SourceBox.Text = Encoding.UTF8.GetString(Bytes);
+        }
+
+        private void EncodeButton_Click(object sender, RoutedEventArgs e)
+        {
+            String Text = SourceBox.Text.Trim();
+            Byte[] Bytes = Encoding.UTF8.GetBytes(Text);
+            Base64Box.Text = Convert.ToBase64String(Bytes);
+        }
+    }
+}
